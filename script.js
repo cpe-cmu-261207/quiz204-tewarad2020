3f808e37a58230fa93942f08cc7843e892f4a4e9
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-07-29 14:17:51 +0700

    notfinish

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ const color = document.querySelector('#color')
 
 // define more constants and variables here
 
+let i = -1
+author.innerHTML = '6306100731 TEWARAD SOMRAD'
 btn_toggle.onclick = () => {
-  // your code here
+  if (i = -1) {
+    let num = 630610731 - 5
+    author.innerHTML = num
+  }else {
+    author.innerHTML = '6306100731 TEWARAD SOMRAD'
+  }
 }
 
-// more codes for Search and Reset buttons here
+const text = document.querySelector('#text')
+ while (text.textContent !== null) {
+  if (text.textContent !== '') {}
+}
